89691968ba603edb6f0ba47d38469075ebad5e3a
Alina <alina-13@bk.ru>
2022-01-20 03:53:24 +0300

    test without cors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { serverError } = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-app.use(cors);
+// app.use(cors);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
