ab99de047542d07e72b8223f9767d452ecb4cfb5
Alina <alina-13@bk.ru>
2022-01-29 23:19:35 +0300

    update package-lock

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
-// const path = require('path');
 require('dotenv').config();
 
+// const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
